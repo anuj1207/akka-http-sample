6b79061e5d3e4b4a3f2d57a225ad20f2cd245c83
Himanshu Rajput <himanshu.rajput@hpe.com>
2018-05-18 15:28:21 +0530

    cleanup

--- src/main/scala/com/combat/code/services/DBservice.scala
+++ src/main/scala/com/combat/code/services/DBservice.scala
@@ -2,21 +2,31 @@ package com.combat.code.services
 
 import java.time.LocalDate
 
-import com.combat.code.models.{Booking, Slot}
-import com.combat.code.repos.BookingRepo
+import com.combat.code.models.{Booking, Slot, Station}
+import com.combat.code.repos.{BookingRepo, StationRepo}
 
 import scala.concurrent.Await
 import scala.concurrent.duration.Duration
 
 class DBservice {
 
-  def findAllBookings():List[Booking]={
-    val bookings=Await.result(BookingRepo.getAllBookings,Duration.Inf)
+  def availableSlotsAtStation(station_id: Int, date: LocalDate): List[Slot] = {
+    val bookingsForDate = findAllBookings().filter(booking => booking.date == date.toString && booking.station_id == station_id)
+
+  }
+
+  def findAllBookings(): List[Booking] = {
+    val bookings = Await.result(BookingRepo.getAllBookings, Duration.Inf)
     bookings
   }
 
-  def availableSlotsAtStation(station_id: Int, date: LocalDate):List[Slot] = {
-    val bookingsForDate = findAllBookings.filter(booking => booking.date == date && booking.station_id == station_id)
-
+  def getStationById(station_id: Int): Option[Station] = {
+    val station = Await.result(StationRepo.getAllStations, Duration.Inf).filter(_.station_Id == station_id)
+    if (station != Nil)
+      None
+    else
+      station.headOption
   }
+
+  getBookingByStationAndDate()
 }
